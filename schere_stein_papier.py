6347dd82ac6b20e53988d9f13c4fdc010f9d829f
Donald Donchi Fofack <donald.donchi.fofack@mni.thm.de>
2021-01-28 21:24:55 +0100

    first commit

---
+++ schere_stein_papier.py
@@ -0,0 +1,55 @@
+import random
+import time
+
+#Einleitung
+print("***************************"); time.sleep(0.5)
+print("* SCHERE | STEIN | PAPIER *"); time.sleep(0.5)
+print("***************************"); time.sleep(0.5)
+
+
+#variable
+figuren= ("Schere", "Stein", "Papier")
+spielen = True
+
+
+while spielen:
+    #Spielerfigur auswählen
+    spielerauswahl = 0
+    while spielerauswahl not in (1,2,3):
+        spielerauswahl = int(input("[1] Schere [2]Stein [3]Papier\n"))
+    spielerfigur = figuren[spielerauswahl - 1]
+
+    #Computerfigur auswählen
+    computerfigur = figuren[random.randint(0, 2)]
+    
+    #Sieger ermitteln
+    if spielerfigur == computerfigur:
+        print("Unentschieden! Computer wählte", computerfigur)
+    else:
+        if spielerfigur == "Schere":
+            if computerfigur == "Stein":
+                print("Verloren! Computer wählte", computerfigur)
+            else:
+                print("Gewonnen! Computer wählte", computerfigur)
+        
+        if spielerfigur == "Stein":
+            if computerfigur == "Schere":
+                print("Gewinnen! Computer wählte", computerfigur)
+            else:
+                print("Verloren! Computer wählte", computerfigur)
+
+        if spielerfigur == "Papier":
+            if computerfigur == "Schere":
+                print("Verloren! Computer wählte", computerfigur)
+            else:
+                print("Gewonnen! Computer wählte", computerfigur)
+
+    #Restart?
+    time.sleep(1)
+    entscheidung =""
+    while entscheidung not in ("y", "n"):
+        entscheidung = input("\nNormal spielen? [y]Ja  [n]Nein \n")
+
+    if(entscheidung == "n"):
+        spielen = False
+
